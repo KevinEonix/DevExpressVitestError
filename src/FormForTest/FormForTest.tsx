13404333f4fc9aa8d5935520a9ae4fea98a033e9
kgo <kgo@eonix.be>
2024-05-13 12:20:16 +0200

    commenting

--- src/FormForTest/FormForTest.tsx
+++ src/FormForTest/FormForTest.tsx
@@ -15,21 +15,21 @@ function FormForTest() {
   <form onSubmit={handleSubmit(async (data) => {
    console.log('data are', data)
   })}>
-   <Controller
-    control={control}
-    name="test"
-    render={({field: {onChange, value}}) => (
-        <DevExpressDatePickerField
-            label="Date field test"
-            id="date-input"
-            format="dd/MM/yyyy"
-            onDateChange={(d) => {
-             onChange(dayjs(d).format('YYYY-MM-DD'));
-            }}
-            value={value}
-        />
-    )}
-   />
+   {/*<Controller*/}
+   {/* control={control}*/}
+   {/* name="test"*/}
+   {/* render={({field: {onChange, value}}) => (*/}
+   {/*     <DevExpressDatePickerField*/}
+   {/*         label="Date field test"*/}
+   {/*         id="date-input"*/}
+   {/*         format="dd/MM/yyyy"*/}
+   {/*         onDateChange={(d) => {*/}
+   {/*          onChange(dayjs(d).format('YYYY-MM-DD'));*/}
+   {/*         }}*/}
+   {/*         value={value}*/}
+   {/*     />*/}
+   {/* )}*/}
+   {/*/>*/}
    <Controller
     control={control}
     name="userEventTest"
